10ea76c7422bddae9f1d505b754fee002bbce8cd
buleowl <buleowl@hotmail.com>
2021-12-03 23:47:49 +0800

    加入專案檔案。

---
+++ ChatCoreTest/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace ChatCoreTest
+{
+  internal class Program
+  {
+    private static byte[] m_PacketData;
+    private static uint m_Pos;
+    
+
+    public static void Main(string[] args)
+    {
+      m_PacketData = new byte[1024];
+      m_Pos = 0;
+
+      Write(109);
+      Write(109.99f);
+      Write("Hello!");
+
+      Console.Write($"Output Byte array(length:{m_Pos}): ");
+      for (var i = 0; i < m_Pos; i++)
+      {
+        Console.Write(m_PacketData[i] + ", ");
+      }
+            Array.Reverse(m_PacketData);
+            Console.WriteLine(BitConverter.ToInt32(m_PacketData, 1020));
+            Console.WriteLine( BitConverter.ToSingle(m_PacketData, 1016)); 
+            Console.WriteLine( Encoding.Unicode.GetString (test.Slice<byte>(m_PacketData,1000,1012))); 
+
+            Console.ReadLine();
+
+    }
+
+    // write an integer into a byte array
+    private static bool Write(int i)
+    {
+      // convert int to byte array
+      var bytes = BitConverter.GetBytes(i);
+      _Write(bytes);
+      return true;
+    }
+
+    // write a float into a byte array
+    private static bool Write(float f)
+    {
+      // convert int to byte array
+      var bytes = BitConverter.GetBytes(f);
+      _Write(bytes);
+      return true;
+    }
+
+    // write a string into a byte array
+    private static bool Write(string s)
+    {
+      // convert string to byte array
+      var bytes = Encoding.Unicode.GetBytes(s);
+
+      // write byte array length to packet's byte array
+      if (Write(bytes.Length) == false)
+      {
+        return false;
+      }
+
+      _Write(bytes);
+      return true;
+    }
+
+    // write a byte array into packet's byte array
+   
+    private static void _Write(byte[] byteData) //反轉資料排列順序寫入
+    {
+      // converter little-endian to network's big-endian
+      if (BitConverter.IsLittleEndian)
+      {
+        Array.Reverse(byteData);
+      }
+
+      byteData.CopyTo(m_PacketData, m_Pos);
+      m_Pos += (uint)byteData.Length;
+    }
+
+
+    }
+    static class test
+    {
+        public static T[] Slice<T>(this T[] source, int start, int end)
+        {
+            // Handles negative ends.
+            if (end < 0)
+            {
+                end = source.Length + end;
+            }
+            int len = end - start;
+
+            // Return new array.
+            T[] res = new T[len];
+            for (int i = 0; i < len; i++)
+            {
+                res[i] = source[i + start];
+            }
+            return res;
+        }
+    }
+}
